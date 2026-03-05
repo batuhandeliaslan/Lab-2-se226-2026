149c61d7f8293fcba39c0bee20e0db3ec3fcb16f
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-03-05 13:19:40 +0300

    Update Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -1,54 +1,44 @@
 #include <iostream>
+using namespace std;
 
 int main() {
-    // Task 1
-    long long n1;
-    std::cout << "Please enter a positive integer greater than 1: ";
-    std::cin >> n1;
-
-    if (n1 > 1) {
-        int steps1 = 0;
-        std::cout << n1;
-        while (n1 != 1) {
-            if (n1 % 2 == 0) n1 /= 2;
-            else n1 = 3 * n1 + 1;
-            std::cout << " -> " << n1;
-            steps1++;
+    int n = 0;
+    
+    while (n < 10 || n > 100) {
+        cout << "Please enter a number between 10 and 100: ";
+        cin >> n;
+        if (n < 10 || n > 100) {
+            cout << "Invalid input. ";
         }
-        std::cout << "\nTotal steps: " << steps1 << "\n\n";
     }
 
-    // Task 2
-    int n2;
-    do {
-        std::cout << "Please enter a number between 10 and 100: ";
-        std::cin >> n2;
-        if (n2 < 10 || n2 > 100) std::cout << "Invalid input. ";
-    } while (n2 < 10 || n2 > 100);
+    int fizz_count = 0;
+    int buzz_count = 0;
+    int fizzbuzz_count = 0;
 
-         int f = 0, b = 0, fb = 0;
-    for (int i = 1; i <= n2; i++) {
-        if (i % 7 == 0) continue;
+    for (int i = 1; i <= n; i++) {
+        if (i % 7 == 0) {
+            continue;
+        }
 
         if (i % 3 == 0 && i % 5 == 0) {
-            std::cout << "FizzBuzz" << std::endl;
-            fb++;
+            cout << "FizzBuzz" << endl;
+            fizzbuzz_count++;
         } else if (i % 3 == 0) {
-            std::cout << "Fizz" << std::endl;
-            f++;
+            cout << "Fizz" << endl;
+            fizz_count++;
         } else if (i % 5 == 0) {
-            std::cout << "Buzz" << std::endl;
-            b++;
+            cout << "Buzz" << endl;
+            buzz_count++;
         } else {
-            std::cout << i << std::endl;
+            cout << i << endl;
         }
     }
 
-    std::cout << "--- Summary ---" << std::endl;
-    std::cout << "Fizz count : " << f << std::endl;
-    std::cout << "Buzz count : " << b << std::endl;
-    std::cout << "FizzBuzz count: " << fb << std::endl;
+    cout << "--- Summary ---" << endl;
+    cout << "Fizz count : " << fizz_count << endl;
+    cout << "Buzz count : " << buzz_count << endl;
+    cout << "FizzBuzz count: " << fizzbuzz_count << endl;
 
     return 0;
 }
-
