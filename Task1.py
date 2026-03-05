3b862a2c3ab80c0fddf6347e257f80620a9f0fc5
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-03-05 12:37:57 +0300

    Create Task1.py

---
+++ Task1.py
@@ -0,0 +1,14 @@
+def main ():
+n = int(input("Please enter a positive integer greater than 1: "))
+if  <=1 :
+print("Lütfen 1'den büyük bir sayı giriniz.")
+return
+steps = 0
+print(n, end="") 
+
+while n ! =1:
+if n % 2 == 0:
+n=n // 2
+else:
+n = 3 * n + 1
+print(f"
