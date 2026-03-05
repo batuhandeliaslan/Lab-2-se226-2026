1e131e81a09444ddeb996297daaccc6acd64487c
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-03-05 12:55:27 +0300

    Update Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -26,3 +26,29 @@ int main() {
         if (n2 < 10 || n2 > 100) std::cout << "Invalid input. ";
     } while (n2 < 10 || n2 > 100);
 
+         int f = 0, b = 0, fb = 0;
+    for (int i = 1; i <= n2; i++) {
+        if (i % 7 == 0) continue;
+
+        if (i % 3 == 0 && i % 5 == 0) {
+            std::cout << "FizzBuzz" << std::endl;
+            fb++;
+        } else if (i % 3 == 0) {
+            std::cout << "Fizz" << std::endl;
+            f++;
+        } else if (i % 5 == 0) {
+            std::cout << "Buzz" << std::endl;
+            b++;
+        } else {
+            std::cout << i << std::endl;
+        }
+    }
+
+    std::cout << "--- Summary ---" << std::endl;
+    std::cout << "Fizz count : " << f << std::endl;
+    std::cout << "Buzz count : " << b << std::endl;
+    std::cout << "FizzBuzz count: " << fb << std::endl;
+
+    return 0;
+}
+
