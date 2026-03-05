77674e20d9c1703b03f7ee0639b58dbf290e5d9a
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-03-05 13:17:13 +0300

    Update Task1.py

--- Task1.py
+++ Task1.py
@@ -1,20 +1,23 @@
-def main ():
-n = int(input("Please enter a positive integer greater than 1: "))
-if  <=1 :
-print("Lütfen 1'den büyük bir sayı giriniz.")
-return
-steps = 0
-print(n, end="") 
+def main():
+    n = int(input("Please enter a positive integer greater than 1: "))
 
-while n ! =1:
-if n % 2 == 0:
-n=n // 2
-else:
-n = 3 * n + 1
-print(f" -> {n}", end="")
-        steps += 1
+    if n <= 1:
+        print("Lütfen 1'den büyük bir sayı giriniz.")
+        return
+
+    steps = 0
+    print(n, end="")
+
+    while n != 1:
+        if n % 2 == 0:
+            n = n // 2
+        else:
+            n = 3 * n + 1
+        
+        print(" ->", n, end="")
+        steps = steps + 1
     
-    print(f"\nTotal steps: {steps}")
+    print("\nTotal steps:", steps)
 
 if __name__ == "__main__":
     main()
