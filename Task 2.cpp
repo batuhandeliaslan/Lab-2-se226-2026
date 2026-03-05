12db32ebee76a625a9ae91b3874d1ca8e0b13c31
Batuhan Deliaslan <153916419+batuhandeliaslan@users.noreply.github.com>
2026-03-05 12:54:03 +0300

    Update Task 2.cpp

--- Task 2.cpp
+++ Task 2.cpp
@@ -1,1 +1,28 @@
+#include <iostream>
+
+int main() {
+    // Task 1
+    long long n1;
+    std::cout << "Please enter a positive integer greater than 1: ";
+    std::cin >> n1;
+
+    if (n1 > 1) {
+        int steps1 = 0;
+        std::cout << n1;
+        while (n1 != 1) {
+            if (n1 % 2 == 0) n1 /= 2;
+            else n1 = 3 * n1 + 1;
+            std::cout << " -> " << n1;
+            steps1++;
+        }
+        std::cout << "\nTotal steps: " << steps1 << "\n\n";
+    }
+
+    // Task 2
+    int n2;
+    do {
+        std::cout << "Please enter a number between 10 and 100: ";
+        std::cin >> n2;
+        if (n2 < 10 || n2 > 100) std::cout << "Invalid input. ";
+    } while (n2 < 10 || n2 > 100);
 
